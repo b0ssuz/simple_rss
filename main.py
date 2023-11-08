0914b75c272ba3e2df9f48c8e20027c3703b75ed
b0ssuz <bs@nessy.online>
2023-11-08 03:15:11 +0100

    added code init

---
+++ main.py
@@ -0,0 +1,28 @@
+from rss import RSS
+import webbrowser
+import subprocess
+import signal
+import sys
+
+def goodbye_message():
+    print("\n \n Goodbye! \n\n")
+    # Add any other cleanup tasks here
+    sys.exit(0)
+
+# Register the goodbye_message function to be called on Ctrl+C
+signal.signal(signal.SIGINT, lambda signal, frame: goodbye_message())
+
+my_feed = RSS()
+
+try:
+        while True:
+            my_feed.list_headlines()
+            user_input_index = int(input("choose article: "))
+            my_feed.read_article_description(user_input_index)
+            user_input_read = input("read full article (y/n): ")
+            if user_input_read == "y":
+                    subprocess.call(["w3m",my_feed.get_article_url(user_input_index)])
+            else:
+                continue
+except KeyboardInterrupt:
+        goodbye_message()
---
+++ rss.py
@@ -0,0 +1,42 @@
+from constants import *
+from rss_parser import Parser
+from requests import *
+from bs4 import BeautifulSoup
+
+
+class RSS():
+    def __init__(self)->None:
+        with open('feeds.lst', 'r') as datei:
+            lines = datei.readlines()
+
+        feeds = [lines.strip() for lines in lines]
+
+        self.feeds_with_content = []
+
+        for feed in feeds:
+            response = get(feed)
+            rss = Parser.parse(response.text)
+            # Iteratively print feed items
+            for item in rss.channel.items:
+                self.feeds_with_content.append(item)
+        
+    def list_headlines(self)->None:
+        index = 0
+        for article in self.feeds_with_content:
+            print(f"{index} - {article.title.content}")
+            index += 1
+
+    def read_article_description(self, index: int)->None:
+        print(f"\n--- {index} \n{self.feeds_with_content[index].title.content} \n")
+        print(f"{self.feeds_with_content[index].description.content} \n")
+    
+    def get_article_url(self, index: int)->str:
+        return self.feeds_with_content[index].link.content
+
+    def read_full_content_as_html(self, index: int)->str:
+        response = get(self.feeds_with_content[index].link.content)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            return(soup.prettify())
+        else:
+            return('Error with statuscode:', response.status_code)
