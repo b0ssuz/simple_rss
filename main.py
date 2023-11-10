91af88ba6cb44696140d495b4e0b90f01dbfab18
Bünyamin Sarikaya <bs@nessy.online>
2023-11-10 17:06:13 +0100

    changed to object oriented style

--- main.py
+++ main.py
@@ -22,66 +22,66 @@ import curses
 import webbrowser
 from rss import RSS
 
-def display_items(stdscr, items, selected, scroll_offset)->None:
-    height, width = stdscr.getmaxyx()
-    num_displayed_items = min(height, len(items))
+class SimpleRSS:
+    def __init__(self):
+        self.my_feed = RSS()
+        self.selected = 0
+        self.scroll_offset = 0
 
-    # Adjust the selected item if it goes out of the visible region
-    if selected < scroll_offset:
-        scroll_offset = selected
-    elif selected >= scroll_offset + num_displayed_items:
-        scroll_offset = selected - num_displayed_items + 1
+    def display_items(self, stdscr):
+        height, width = stdscr.getmaxyx()
+        num_displayed_items = min(height, len(self.items))
 
-    stdscr.clear()
+        if self.selected < self.scroll_offset:
+            self.scroll_offset = self.selected
+        elif self.selected >= self.scroll_offset + num_displayed_items:
+            self.scroll_offset = self.selected - num_displayed_items + 1
 
-    # Print the list of items within the visible region
-    for i in range(num_displayed_items):
-        item_index = i + scroll_offset
-        if item_index < len(items) and i == selected - scroll_offset:
-            stdscr.addstr(i, 0, f"> {items[item_index]}", curses.A_STANDOUT)
-        elif item_index < len(items):
-            stdscr.addstr(i, 0, f"  {items[item_index]}")
+        stdscr.clear()
 
-    stdscr.refresh()
+        for i in range(num_displayed_items):
+            item_index = i + self.scroll_offset
+            if item_index < len(self.items) and i == self.selected - self.scroll_offset:
+                stdscr.addstr(i, 0, f"> {self.items[item_index]}", curses.A_STANDOUT)
+            elif item_index < len(self.items):
+                stdscr.addstr(i, 0, f"  {self.items[item_index]}")
 
-def handle_selected_item(stdscr, my_feed, items, selected)->None:
-    height = stdscr.getmaxyx()[0]
-    stdscr.clear()  # Clear the screen
-    my_feed.mark_as_read(selected)
-    stdscr.addstr(0, 0, items[selected])
-    stdscr.addstr(2, 0, my_feed.get_description(selected))
-    stdscr.addstr(12, 0, my_feed.get_article_url(selected))
-    stdscr.addstr(height - 1, 0, f"Press l or enter to open the article or any other key to go back")
-    key = stdscr.getch()
+        stdscr.refresh()
 
-    while True:
-        if key in [10, ord("l")]:  # Enter or l key to select
-            webbrowser.open(my_feed.get_article_url(selected))
-            return
-        else:
-            return
-
-def main(stdscr):
-    my_feed = RSS()
-    selected = 0
-    scroll_offset = 0  # Added to keep track of scrolling
-
-    while True:
-        items = my_feed.get_headlines(with_read=False)
-
-        display_items(stdscr, items, selected, scroll_offset)
-
+    def handle_selected_item(self, stdscr):
+        height = stdscr.getmaxyx()[0]
+        stdscr.clear()
+        self.my_feed.mark_as_read(self.selected)
+        stdscr.addstr(0, 0, self.items[self.selected])
+        stdscr.addstr(2, 0, self.my_feed.get_description(self.selected))
+        stdscr.addstr(12, 0, self.my_feed.get_article_url(self.selected))
+        stdscr.addstr(height - 1, 0, "Press l or enter to open the article or any other key to go back")
         key = stdscr.getch()
 
-        if key in [curses.KEY_DOWN, ord("j")] and selected < len(items) - 1:
-            selected += 1
-        elif key in [curses.KEY_UP, ord("k")] and selected > 0:
-            selected -= 1
-        elif key in [10, ord("l")]:  # Enter or l key to select
-            handle_selected_item(stdscr, my_feed, items, selected)
-        elif key in [27, ord("q")]:  # Escape key or q to exit
-            break
+        while True:
+            if key in [10, ord("l")]:
+                webbrowser.open(self.my_feed.get_article_url(self.selected))
+                return
+            else:
+                return
+
+    def run(self, stdscr):
+        while True:
+            self.items = self.my_feed.get_headlines(with_read=False)
+
+            self.display_items(stdscr)
+
+            key = stdscr.getch()
+
+            if key in [curses.KEY_DOWN, ord("j")] and self.selected < len(self.items) - 1:
+                self.selected += 1
+            elif key in [curses.KEY_UP, ord("k")] and self.selected > 0:
+                self.selected -= 1
+            elif key in [10, ord("l")]:
+                self.handle_selected_item(stdscr)
+            elif key in [27, ord("q")]:
+                break
 
 if __name__ == "__main__":
-    curses.wrapper(main)
+    curses.wrapper(SimpleRSS().run)
 
